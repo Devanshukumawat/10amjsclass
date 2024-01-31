105d3a3d7d6bef45e3c78e84248474a159e0ba16
Devanshukumawat <devanshukumawat063gmail.com>
2024-01-31 11:15:37 +0530

    add new files

---
+++ scop.js
@@ -0,0 +1,73 @@
+// var a = 10;
+
+// {
+//     var b = 11
+// }
+
+
+// console.log(a)
+// console.log(b)
+
+// let a = 11;
+// var b = "aman"
+// const c = "Dev"
+
+// console.log(a,b,c)
+
+// let a = "Dev"
+
+// {
+//     let a = "Aman"
+//     console.log(a)
+    
+// }
+
+// console.log(a)
+// console.log(b)
+
+// function myFn(){
+//     if(true){
+//         let a = 10;
+//         var b = 11;
+//         const c = 17;
+
+//     }
+//     console.log(a,b,c)
+// }
+
+// myFn()
+
+
+// function add(a,b){
+//     return a*b
+// }
+
+// let c = add(8,9)
+
+// console.log(c)
+
+// function add(){
+ 
+//     var b = 88;
+//     console.log(b)
+   
+// }
+
+// add()
+
+
+// function add(){
+//      let a = 10
+//      return a
+// }
+
+// console.log(add())
+
+
+
+
+
+
+
+
+
