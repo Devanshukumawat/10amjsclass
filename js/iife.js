d187197ba6e3697cc26cf1fb5f915102b1791e6c
Devanshukumawat <devanshukumawat063gmail.com>
2024-02-02 11:24:27 +0530

    Add some new files

---
+++ js/iife.js
@@ -0,0 +1,49 @@
+// function add(){
+//     console.log("Function")
+// }
+
+
+// (function (a,b){
+//     console.log(a+b)
+//     console.log("Db is connected")
+// })(8,9);
+
+// ((c,d)=>{
+//     console.log(c+d)
+// })(7,8);
+
+// (function (){
+//     console.log("HEllo class")
+// })()
+
+// function myFn(){
+//     console.log("Hello i am a function")
+// }
+
+// // setTimeout(myFn,2000)
+
+// // setInterval(myFn,2000)
+
+// setTimeout(myFn,2000)
+
+// let a = 10;
+// let b = "dev"
+// console.log(a)
+// console.log(b)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
